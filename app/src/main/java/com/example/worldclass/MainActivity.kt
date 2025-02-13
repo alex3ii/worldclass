7c0fa43607f458dcb834484ec62be83668834e08
Alejandro Perez <alexurzua322@gmail.com>
2025-02-12 19:30:39 -0600

    box layout and text property

--- app/src/main/java/com/example/worldclass/MainActivity.kt
+++ app/src/main/java/com/example/worldclass/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+/*import androidx.compose.foundation.layout.FlowRowScopeInstance.align*/
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
@@ -20,9 +23,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.worldclass.ui.theme.WorldclassTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,6 +59,9 @@ class MainActivity : ComponentActivity() {
                     }
                 Column(){
                     ModifierExample2()
+                }
+                Column(){
+                    ModifierExample4()
                 }
 
             }
@@ -107,7 +120,48 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+    @Preview (showBackground=true)
+    @Composable
+    fun ModifierExample4(){
+        Box(
+            modifier=Modifier
+                .padding(10.dp)
+                .height(300.dp)
+                .width(300.dp)
+        ){
+            Text("1",Modifier.align(Alignment.TopStart))
+            Text("2",Modifier.align(Alignment.TopCenter))
+            Text("3",Modifier.align(Alignment.TopEnd))
+            Text("4",Modifier.align(Alignment.CenterStart))
+            Text("5",Modifier.align(Alignment.Center))
+            Text("6",Modifier.align(Alignment.CenterEnd))
+            Text("7",Modifier.align(Alignment.BottomStart))
+            Text("8",Modifier.align(Alignment.BottomCenter))
+            Text("9",Modifier.align(Alignment.BottomEnd))
+        }
+
 }
+    @Preview(showBackground = true)
+    @Composable
+    fun CustomText() {
+        Column (){
+            Text(
+                stringResource(R.string.single_text),
+                color = colorResource(R.color.teal_700),
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.ExtraBold
+            )
+            val gradientcolors = listOf(Color.Blue, Color.Green, Color.Cyan, colorResource(R.color.purple_200))
+            Text(
+                stringResource(R.string.single_text),
+                style = TextStyle(brush= Brush.linearGradient(colors=gradientcolors)))
+
+        }
+    }
+        }
+
+
 
 
 @Composable
