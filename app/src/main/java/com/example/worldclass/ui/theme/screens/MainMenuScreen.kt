c3763e26ea3919e1334321ab7c5c73fadc6976e8
Alejandro Perez <alexurzua322@gmail.com>
2025-02-21 07:09:10 -0600

    strings and other things

---
+++ app/src/main/java/com/example/worldclass/ui/theme/screens/MainMenuScreen.kt
@@ -0,0 +1,30 @@
+package com.example.worldclass.ui.theme.screens
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun MainMenuScreen(navController:NavHostController){
+    Column(modifier=Modifier
+        .padding(10.dp)
+        .fillMaxSize()  ) {Text("main menu")
+    Button(
+        onClick = {navController.navigate("home_screen")}
+    )
+     {Text("go to home screen") }
+        Button(
+            onClick = {navController.navigate("TextScreen")}
+        )
+        {Text("go to test screen") }
+
+    }
+
+    }
