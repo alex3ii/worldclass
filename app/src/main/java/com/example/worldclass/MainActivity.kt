dcd5053e73b9e4ffe19880299ac4cad2efc97d2f
Alejandro Perez <alexurzua322@gmail.com>
2025-03-02 03:19:15 -0600

    interfaz de inicio cinepolis

--- app/src/main/java/com/example/worldclass/MainActivity.kt
+++ app/src/main/java/com/example/worldclass/MainActivity.kt
@@ -10,7 +10,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.worldclass.ui.theme.WorldclassTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +39,209 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WorldclassTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+         CinepolisApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CinepolisApp() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+
+
+        // Cartelera
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 0.dp)
+                .background(Color.Blue),
+
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                "Cartelera",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color=Color.White
+            )
+        }
+
+        // Películas
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical =0.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.estrenos),
+                    contentDescription = "estrenos",
+                    modifier = Modifier.size(350.dp).height(0.dp),
+                    contentScale = ContentScale.Fit
+
+                )
+                Text(
+                    "",
+                    fontSize = 0.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+
+
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 5.dp)
+                ,
+
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "  Espacio Aguascalientes  ",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                color=Color.White,
+                modifier = Modifier
+                    .background(Color.Cyan)
+            )
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 22.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.capitan_america),
+                    contentDescription = "capitan america",
+                    modifier = Modifier.size(180.dp).height(30.dp),
+                    contentScale = ContentScale.Fit
+
+                )
+                Text(
+                    " ",
+                    fontSize = 0.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.peliculas_2),
+                    contentDescription = "capitan america",
+                    modifier = Modifier.size(180.dp).height(30.dp),
+                    contentScale = ContentScale.Fit
+
+                )
+                Text(
+                    " ",
+                    fontSize = 0.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+//bottom movies
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 22.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.pelicula_1_bottom),
+                    contentDescription = "capitan america",
+                    modifier = Modifier.size(150.dp).height(30.dp),
+                    contentScale = ContentScale.Fit
+
+                )
+                Text(
+                    " ",
+                    fontSize = 0.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.pelicula_2_bottom),
+                    contentDescription = "capitan america",
+                    modifier = Modifier.size(150.dp).height(30.dp),
+                    contentScale = ContentScale.Fit
+
+                )
+                Text(
+                    " ",
+                    fontSize = 0.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.pelicula_3_bottom),
+                    contentDescription = "capitan america",
+                    modifier = Modifier.size(150.dp).height(30.dp),
+                    contentScale = ContentScale.Fit
+
+                )
+                Text(
+                    " ",
+                    fontSize = 0.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+
+        BottomAppBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray),
+            contentPadding = PaddingValues(0.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Home, contentDescription = "Películas")
+                }
+                Text("Películas")
+                Text("Alimentos")
+                Text("Club")
+                Text("Cines")
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.AccountCircle, contentDescription = "Perfil")
+                }
+                Text("Perfil")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WorldclassTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    CinepolisApp()
 }
